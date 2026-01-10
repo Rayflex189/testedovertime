bd1ca4f78ea1a5a52001b7e4d810a2c5ea73e4d2
Coder Black <roryisaiah66@gmail.com>
2026-01-10 03:47:08 -0800

    save

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+
 from . import views
+from django.views.generic import TemplateView
 
 app_name = 'accounts'
 
 urlpatterns = [
     # Authentication
-    path('/', auth_views.homeView.as_view(template_name='accounts/index.html'), name='home'),
+    path('', TemplateView.as_view(template_name='accounts/index.html'), name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('register/', views.register, name='register'),

2cde4a58830c1235cd3ba387f3a3e77bec56a0d3
Coder Black <165771215+Rayflex189@users.noreply.github.com>
2026-08-13 13:20:07 +0100

    Update settings.py
    Save

--- ecommerce_project/settings.py
+++ ecommerce_project/settings.py
@@ -70,11 +70,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ecommerce_project.wsgi.application'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": 'postgres',
+        "USER": 'postgres.odjccuyeuapflvmxvhmm',
+        "PASSWORD": 'daWOjYb5f9SsJuyx',
+        "HOST": 'aws-0-us-west-1.pooler.supabase.com',
+        "PORT": '6543',
+        "CONN_MAX_AGE": 60,
     }
-}
+} 
 
 AUTH_PASSWORD_VALIDATORS = [
     {
